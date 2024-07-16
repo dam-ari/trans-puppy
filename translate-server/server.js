7588f4a8724e52f18c9bfdcfc29e683980632dfe
Bareket <bareket.damari@live.biu.ac.il>
2024-07-16 14:58:23 +0300

    chore: Remove unused code and update dependencies

--- translate-server/server.js
+++ translate-server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
 
 app.use(express.json());
 
